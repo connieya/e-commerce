66a16ede59fe077eb1c31b4f5013cce15a62ad02
connieya <gunny6026@naver.com>
2023-12-03 20:31:50 +0900

    Initial commit

---
+++ src/main/java/com/example/hanghaeplus/HanghaePlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaeplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaePlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hanghaeplus/HanghaePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanghaeplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
