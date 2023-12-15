0f02da5325545502d807d35c270113eba8118977
connieya <gunny6026@naver.com>
2023-12-15 09:25:35 +0900

    test : 잔액 충전 테스트

--- src/test/java/com/example/hanghaeplus/orm/repository/PointRepositoryTest.java
+++ src/test/java/com/example/hanghaeplus/orm/repository/PointRepositoryTest.java
@@ -5,10 +5,13 @@ import com.example.hanghaeplus.orm.entity.Point;
 import com.example.hanghaeplus.orm.entity.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,10 +25,19 @@ class PointRepositoryTest {
     @Autowired
     PointRepository pointRepository;
 
-    @AfterEach
-    void tearDown() {
-        pointRepository.deleteAll();
-        userRepository.deleteAll();
+//    @AfterEach
+//    void tearDown() {
+//        pointRepository.deleteAll();
+//        userRepository.deleteAll();
+//    }
+
+    @BeforeEach
+    void before(){
+        User user = new User("건희");
+        // 포인트 0원으로 초기화
+        user.addPoint();
+        userRepository.save(user);
+
     }
 
 
@@ -34,22 +46,18 @@ class PointRepositoryTest {
     void rechargePoint() {
         // given
         // 사용자 등록
-        User user = new User("건희");
-        // 포인트 0원으로 초기화
-        user.addPoint();
-
-        User savedUser = userRepository.save(user);
+        Optional<User> user = userRepository.findByName("건희");
 
         // 사용자와 충전할 금액
         PointPostRequest request = PointPostRequest
                 .builder()
-                .username(user.getName())
+                .username(user.get().getName())
                 .point(50000L)
                 .build();
 
 
         // when
-        Point point = pointRepository.findByUserId(savedUser.getId());
+        Point point = pointRepository.findByUserId(user.get().getId());
         point.recharge(request.getPoint());
         Point rechargedPoint = pointRepository.save(point);
 
