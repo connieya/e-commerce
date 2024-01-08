9b7543a84fd11eeaf96705cb9624b17bc94002d7
connieya <gunny6026@naver.com>
2024-01-08 20:39:02 +0900

    refactor : PointManager 클래스 제거

--- src/main/java/com/example/hanghaeplus/service/order/OrderService.java
+++ src/main/java/com/example/hanghaeplus/service/order/OrderService.java
@@ -1,7 +1,6 @@
 package com.example.hanghaeplus.service.order;
 
 import com.example.hanghaeplus.component.order.OrderAppender;
-import com.example.hanghaeplus.component.point.PointManager;
 import com.example.hanghaeplus.controller.order.request.OrderPostRequest;
 import com.example.hanghaeplus.controller.order.response.OrderPostResponse;
 import com.example.hanghaeplus.repository.order.Order;
@@ -23,25 +22,16 @@ public class OrderService {
     private final ProductService productService;
     private final UserService userService;
     private final OrderAppender orderAppender;
-    private final PointManager pointManager;
     private final PaymentService paymentService;
     private final ApplicationEventPublisher publisher;
-
 
     @Transactional
     public OrderPostResponse create(OrderPostRequest request) {
         User user = userService.findById(request.getUserId());
         productService.deduct(request);
-        // 주문
         Order savedOrder = orderAppender.append(user, request.getProducts());
-        // 결제
         paymentService.execute(savedOrder, user);
-        // 포인트 내역 저장
-        pointManager.process(user, savedOrder);
         publisher.publishEvent(new OrderEvent(this, savedOrder));
         return OrderPostResponse.of(savedOrder);
     }
-
-
-
 }
--- src/main/java/com/example/hanghaeplus/service/payment/PaymentService.java
+++ src/main/java/com/example/hanghaeplus/service/payment/PaymentService.java
@@ -3,6 +3,8 @@ package com.example.hanghaeplus.service.payment;
 import com.example.hanghaeplus.repository.order.Order;
 import com.example.hanghaeplus.repository.payment.Payment;
 import com.example.hanghaeplus.repository.payment.PaymentRepository;
+import com.example.hanghaeplus.repository.point.Point;
+import com.example.hanghaeplus.repository.point.PointRepository;
 import com.example.hanghaeplus.repository.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentRepository paymentRepository;
+    private final PointRepository pointRepository;
 
     public void execute(Order order , User user) {
         user.deductPoints(order.getTotalPrice());
+        // 포인트 사용 내역
+        Point point = Point.create(user, order.getTotalPrice());
+        pointRepository.save(point);
+        // 결제
         Payment payment = new Payment(order, user);
         paymentRepository.save(payment);
     }
