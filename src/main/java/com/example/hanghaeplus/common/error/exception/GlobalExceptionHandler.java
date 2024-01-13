372f2351fe76e59c846df3859499222e37f9ebd0
connieya <gunny6026@naver.com>
2024-01-13 16:41:38 +0900

    feat : 로그 출력 범위 지정

--- src/main/java/com/example/hanghaeplus/common/error/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hanghaeplus/common/error/exception/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.example.hanghaeplus.common.error.exception;
 
 import com.example.hanghaeplus.common.error.ErrorCode;
 import com.example.hanghaeplus.common.error.ErrorResponse;
+import com.example.hanghaeplus.common.web.filter.LogFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,10 +19,12 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(LogFilter.class);
     @ExceptionHandler
     protected ResponseEntity<?> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = ErrorResponse.of(errorCode, e.getErrors());
+        LOGGER.error(e.getMessage(), errorCode.getMessage());
         return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
     }
 
@@ -27,12 +32,14 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
             MissingServletRequestParameterException e) {
         final ErrorResponse response = ErrorResponse.of(INPUT_VALUE_INVALID, e.getParameterName());
+        LOGGER.error(e.getMessage());
         return new ResponseEntity<>(response, BAD_REQUEST);
     }
 
     @ExceptionHandler
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         final ErrorResponse response = ErrorResponse.of(INPUT_VALUE_INVALID, e.getBindingResult());
+        LOGGER.error(e.getMessage());
         return new ResponseEntity<>(response, BAD_REQUEST);
     }
 
@@ -40,6 +47,7 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
             MethodArgumentTypeMismatchException e) {
         final ErrorResponse response = ErrorResponse.of(e);
+        LOGGER.error(e.getMessage());
         return new ResponseEntity<>(response, BAD_REQUEST);
     }
 
